72ef38b38c50fd697e5f45e7dd030a2ff91a04ab
Komal6627 <komalkilledar1996@gmail.com>
2023-05-29 19:07:25 +0530

    Todo-app done

--- backend/routes/TodoRoute.js
+++ backend/routes/TodoRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createList, deleteList, getList, updateList,  } from "../controllers/TodoControllers.js";
+import { createList,  deleteList, getList, updateList,  } from "../controllers/TodoControllers.js";
 
 const router = express.Router();
 
